649017ce351c08d274d11c9fabc04780b1bfff6d
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-05-29 01:19:07 -0400

    swagger añadido y configurado

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { initSwagger } from './app.swagger';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initSwagger(app);
   const port = 3000;
   const logger = new Logger();
   logger.log('Servidor corriendo en el puerto ' + port);
