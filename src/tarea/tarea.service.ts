9b927ca3a4149aef9080c0072cdb79061735074c
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-05-29 08:53:20 -0400

    funcionalidad del servicio de tarea completo

--- src/tarea/tarea.service.ts
+++ src/tarea/tarea.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+
+import { CreateTareaDto, EditTareaDto } from './dtos';
+import { Tarea, TareaDocument } from './schemas';
 
 @Injectable()
-export class TareaService {}
+export class TareaService {
+  constructor(
+    @InjectModel(Tarea.name)
+    private tareaModel: Model<TareaDocument>,
+  ) {}
+
+  async getAllTask(): Promise<Tarea[]> {
+    return await this.tareaModel.find().exec();
+  }
+
+  async createTask(createTareaDto: CreateTareaDto) {
+    const tareaCreada = new this.tareaModel(createTareaDto);
+    tareaCreada.save();
+    return {
+      message: 'Tarea creada exitosamente',
+      data: tareaCreada,
+    };
+  }
+
+  async getOneTask(_id: number) {
+    const tarea = await this.tareaModel.findById(_id).exec();
+    if (!tarea) new NotFoundException('La tarea no existe');
+    return tarea;
+  }
+
+  async editTask(_id: number, editTareaDto: EditTareaDto) {
+    const tareaUpdate = await this.tareaModel.findByIdAndUpdate(
+      _id,
+      editTareaDto,
+      { new: true },
+    );
+    if (!tareaUpdate) new NotFoundException('La tarea no se puedo actualizar');
+    return {
+      message: 'Tarea actualizada correctamente',
+      data: tareaUpdate,
+    };
+  }
+
+  async deleteTask(_id: number) {
+    const tareaDelete = await this.tareaModel.findByIdAndRemove(_id);
+    return {
+      message: 'Tarea eliminada correctamen',
+      data: tareaDelete,
+    };
+  }
+}
